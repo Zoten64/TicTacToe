0a83888518e2d3040e3b6ef0aeeb61fdcdca13e4
Zoten64 <forjavligratta@gmail.com>
2024-01-11 00:01:00 +0100

    Feat: Add function to store what grid items has been clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 console.log("Connected!");
 
+// Global variables
+
+//Possible win combinations
+let combinations = []
+//The grid items X has claimed
+let xCombinations = []
+//The grid items O has claimed
+let oCombinatioms = []
+
 document.addEventListener("DOMContentLoaded", function(){
     console.log("Loaded")
 
@@ -9,7 +18,17 @@ document.addEventListener("DOMContentLoaded", function(){
         item.addEventListener("click", function(){
             let clickedItem = item.getAttribute("grid-number");
             console.log(clickedItem)
-            document.getElementsByClassName("game-grid-item")[parseInt(clickedItem - 1)].children[0].innerText = "x";
+            document.getElementsByClassName("game-grid-item")[parseInt(clickedItem - 1)].children[0].innerText = "X";
+            addXToCombo(clickedItem);
         })
     }
 })
+
+function addXToCombo(clickedItem){
+    xCombinations.push(parseInt(clickedItem))
+    console.log(xCombinations)
+};
+
+function checkWin(clickedItem){
+    
+};
