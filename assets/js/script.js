525de94c18a050ee38347902c77a8cfc9a4879a4
Zoten64 <forjavligratta@gmail.com>
2024-01-11 00:17:59 +0100

    Feat: Make the checkWin function functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,14 @@ console.log("Connected!");
 // Global variables
 
 //Possible win combinations
-let combinations = []
+let combinations = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
 //The grid items X has claimed
-let xCombinations = []
+let xCombinations = [];
 //The grid items O has claimed
-let oCombinatioms = []
+let oCombinatioms = [];
 
 document.addEventListener("DOMContentLoaded", function(){
-    console.log("Loaded")
+    console.log("Loaded");
 
     let gridItem = document.getElementsByClassName("game-grid-item");
 
@@ -18,17 +18,32 @@ document.addEventListener("DOMContentLoaded", function(){
         item.addEventListener("click", function(){
             let clickedItem = item.getAttribute("grid-number");
             console.log(clickedItem)
+            //Adds an X to the clicked grid item
             document.getElementsByClassName("game-grid-item")[parseInt(clickedItem - 1)].children[0].innerText = "X";
             addXToCombo(clickedItem);
+            checkWin();
         })
     }
 })
 
+/**
+ * Adds the id of the grid clicked to the array that keeps
+ * track of the player's inputs
+ */
 function addXToCombo(clickedItem){
-    xCombinations.push(parseInt(clickedItem))
-    console.log(xCombinations)
+    xCombinations.push(parseInt(clickedItem));
+    console.log(xCombinations);
 };
 
-function checkWin(clickedItem){
-    
+/**
+ * Checks if one of the combinations has been reached
+ */
+function checkWin(){
+    for(let combo of combinations){
+        if(xCombinations.includes(combo[0]) && xCombinations.includes(combo[1]) && xCombinations.includes(combo[2])){
+            alert("You won")
+            //Clears array
+            xCombinations.length = 0;
+        };
+    };
 };
