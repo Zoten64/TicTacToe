7fc5160ef4fd96b592654831d3c3ea270b732065
Zoten64 <forjavligratta@gmail.com>
2024-01-10 23:38:25 +0100

    Feat: Add identifier for which grid item is pressed. Add placeholder for changing the inner text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log("Connected!")
+console.log("Connected!");
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Loaded")
+
+    let gridItem = document.getElementsByClassName("game-grid-item");
+
+    for(let item of gridItem){
+        item.addEventListener("click", function(){
+            let clickedItem = item.getAttribute("grid-number");
+            console.log(clickedItem)
+            document.getElementsByClassName("game-grid-item")[parseInt(clickedItem - 1)].innerText = "x";
+        })
+    }
+})
